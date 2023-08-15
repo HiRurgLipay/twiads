eb73364b5d5e07f6a0dbc8860df67a7349180691
HiRurgLipay <sl38lipay@gmail.com>
2023-08-15 16:15:17 +0300

    Linters checks

--- scr/twiads/core/models/notification.py
+++ scr/twiads/core/models/notification.py
@@ -9,7 +9,7 @@ class Notification(BaseModel):
     text = models.CharField(max_length=500, null=False)
 
     user = models.ManyToManyField(to="User", related_name="notifications")
-    notification_type = models.ForeignKey(to="notification_type", on_delete=models.CASCADE, related_name="notification_types")
+    notification_type = models.ForeignKey(to="NotificationType", on_delete=models.CASCADE, related_name="notification_types")
 
     class Meta:
         db_table = "notifications"
