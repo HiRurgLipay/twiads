c0f54d2cae64815bcda9ece3a301cb401d79bc05
HiRurgLipay <sl38lipay@gmail.com>
2023-08-15 15:56:07 +0300

    Linters checks

--- scr/twiads/core/models/notification.py
+++ scr/twiads/core/models/notification.py
@@ -1,17 +1,15 @@
 from django.db import models
+
 from .base_model import BaseModel
 
 
 class Notification(BaseModel):
-    '''Notification to user from admin or activity'''
+    """Notification to user from admin or activity"""
+
     text = models.CharField(max_length=500, null=False)
-    
-    user = models.ManyToManyField(to='User', 
-                                  related_name='notifications')
-    notification_type = models.ForeignKey(to='notification_type',
-                                          on_delete=models.CASCADE,
-                                          related_name='notification_types')
+
+    user = models.ManyToManyField(to="User", related_name="notifications")
+    notification_type = models.ForeignKey(to="notification_type", on_delete=models.CASCADE, related_name="notification_types")
 
     class Meta:
-        db_table = 'notifications'
-        
+        db_table = "notifications"
